beaf1dd839ba0e3019c7d9f77495a0184732f56a
Bella Fernandez <bellafernandez@Bellas-MacBook-Air.local>
2019-09-20 13:01:05 -0700

    erros fixed

--- config/connection.js
+++ config/connection.js
@@ -2,9 +2,9 @@ var mysql = require("mysql");
 
 var connection = mysql.createConnection({
   host: "localhost",
-  port: 3000,
+  port: 3306,
   user: "root",
-  password: "",
+  password: "password",
   database: "burgers_db"
 });
 
